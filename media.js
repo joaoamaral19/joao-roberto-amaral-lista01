906d96fc88921f50ea8de51910d9cc3c1d9e2de5
joaoamaral19 <jr71delas@gmail.com>
2026-08-27 12:56:19 -0400

    Create media.js

---
+++ media.js
@@ -0,0 +1,10 @@
+let numeros = [7, 8, 6, 9, 10];
+
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+}
+let media = soma / numeros.length;
+
+console.log("A média dos valores é: " + media);
